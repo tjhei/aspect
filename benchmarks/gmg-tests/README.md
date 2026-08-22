239c276e570b7c9ff9d588271a56947910ace85f
Timo Heister <timo.heister@gmail.com>
2026-08-22 16:53:02 -0400

    update

---
+++ benchmarks/gmg-tests/README.md
@@ -0,0 +1,11 @@
+
+Test 1: box-3d-bc/
+
+3d box (nsinker) with various combinations of boundary conditions:
+
+no-slip: all 6 sides no slip
+free-slip: all sides free slip
+partial-free-slip: left and front free slip, rest no slip
+partial-set: left: free slip, front: x set to smooth function, other: no slip
+
+
---
+++ benchmarks/gmg-tests/box-3d-bc/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+NP=${NP:-8}
+
+for method in GMG AMG; do
+  for boundary_case in no-slip free-slip partial-free-slip partial-set; do
+    cat "${boundary_case}.prm" > current.prm
+
+    if [[ ${method} == AMG ]]; then
+      cat amg.prm >> current.prm
+    fi
+
+    mpirun -n "${NP}" ./aspect-release current.prm \
+      | tee "log-${method}-${boundary_case}.txt"
+  done
+done
+
+
