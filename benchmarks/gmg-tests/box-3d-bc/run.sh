330641e915fbc2300b0d59de51f88c1d85c665f8
Timo Heister <timo.heister@gmail.com>
2026-09-08 10:38:59 -0400

    add GMG-LS

--- benchmarks/gmg-tests/box-3d-bc/run.sh
+++ benchmarks/gmg-tests/box-3d-bc/run.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 
 NP=${NP:-8}
 
-for method in GMG AMG; do
+for method in GMG-GC GMG-LS AMG; do
   for boundary_case in no-slip free-slip partial-free-slip partial-set open periodic; do
     cat "${boundary_case}.prm" > current.prm
 
     if [[ ${method} == AMG ]]; then
       cat amg.prm >> current.prm
+    elif [[ ${method} == GMG-LS ]]; then
+      cat gmg-local-smoothing.prm >> current.prm
     fi
 
     mpirun -n "${NP}" ./aspect-release current.prm \
